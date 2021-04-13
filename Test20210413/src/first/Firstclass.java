b04c73f6204d08610e60d2e4170c9faae9d49a98
Ktc User <Ktc User@192.168.219.106>
2021-04-13 09:14:23 +0900

    프린트문 수정

--- Test20210413/src/first/Firstclass.java
+++ Test20210413/src/first/Firstclass.java
@@ -5,7 +5,7 @@ public class Firstclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git");
-		System.out.println("첫번째 클래스");
+		System.out.println("지우고 다시씀");
 	}
 
 }
